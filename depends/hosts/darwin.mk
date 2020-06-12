2c83d03a7faa2b20a100c9ebeeb6efd15ed0826e
Overcooked Panda <overcookedpanda@gmail.com>
2020-06-11 23:31:01 -0500

    CI: Fix MacOS Build

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,5 +1,5 @@
 OSX_MIN_VERSION=10.12
-OSX_SDK_VERSION=10.12
+OSX_SDK_VERSION=10.14
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
@@ -15,4 +15,3 @@ darwin_debug_CFLAGS=-O1
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
 darwin_native_toolchain=native_cctools
-darwin_cmake_system=Darwin
